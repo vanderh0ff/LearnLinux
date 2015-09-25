be168e908316091d0b9a0066b6cfcde9066c529e
Matthew Vander Hoff <h0ffline@localhost.localdomain>
2015-09-24 20:24:53 -0400

    added a help script for move exercises

---
+++ bash/move_excersizes/help.sh
@@ -0,0 +1,10 @@
+echo "are you sure you want help?"
+echo "or would you rather figure it out on your own?"
+echo "type y to get help, or n to exit"
+read HELP
+case $HELP in 
+    [yY]) echo "you have to escape space's with a backslash \\ ";
+        echo "or try using tab completeion";;
+    [nN]) echo "google is your firend ";;
+esac
+
